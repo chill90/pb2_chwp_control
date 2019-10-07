60ba864348d428b924315b0c7e269b521075b47e
Charles Hill <charles.a.hill01@gmail.com>
2019-10-07 11:08:21 -0700

    Adding config file to define exp

---
+++ config/exp_config.py
@@ -0,0 +1,5 @@
+# On which experiment are you operating a CHWP?                                                                                                                                                            
+# Options (as of 2019-08-29)                                                                                                                                                                               
+# 'SO' = Simons Observatory                                                                                                                                                                                
+# 'PB2b' = POLARBEAR-2b                                                                                                                                                                                    
+exp = 'SO'
